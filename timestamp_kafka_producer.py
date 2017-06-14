cfc1b8dea4fa2481a0b8c1fa93476f08f0c5afa2
jvicentem <j.vicentem@alumnos.urjc.es>
2017-06-14 14:51:17 +0200

    Reading csv file properly

--- timestamp_kafka_producer.py
+++ timestamp_kafka_producer.py
@@ -3,27 +3,9 @@ import random
 import time
 import sys, os
 import logging
-import re
+import csv
 
-def _get_epoch(line_splitted):
-    try:
-        this_tweet_date = line_splitted[7]
-        this_tweet_epoch = time.mktime(time.strptime(this_tweet_date, "%d/%m/%Y %H:%M"))
-    except ValueError:
-        found = False
-
-        i = 7
-
-        while not found and i < len(line_splitted):
-            i += 1
-            found = re.match('2./03/2014 ..:..', line_splitted[i]) != None
-
-        this_tweet_date = line_splitted[i]
-        this_tweet_epoch = time.mktime(time.strptime(this_tweet_date, "%d/%m/%Y %H:%M"))    
-
-    return this_tweet_epoch
-
-if __name__== '__main__':
+if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     try:
         logging.info("Initialization...")
@@ -36,24 +18,19 @@ if __name__== '__main__':
     
         with open(filename, 'rt') as f:
             try:
-                next(f) # skip csv header
+                reader = csv.DictReader(f, delimiter = '\t')
 
-                line = next(f)
+                line = next(reader)
 
                 while line != None:
                     logging.info(line)
 
-
                     try: 
-                        next_line = next(f)
+                        next_line = next(reader)
 
-                        line_splitted = line.split('\t')
+                        this_tweet_epoch = time.mktime(time.strptime(line['PUBDATE'], "%d/%m/%Y %H:%M"))
 
-                        this_tweet_epoch = _get_epoch(line_splitted)
-
-                        line_splitted_next = next_line.split('\t')
-
-                        next_tweet_epoch = _get_epoch(line_splitted_next)
+                        next_tweet_epoch =  time.mktime(time.strptime(next_line['PUBDATE'], "%d/%m/%Y %H:%M"))
 
                         time_to_sleep = abs(next_tweet_epoch - this_tweet_epoch)
 
